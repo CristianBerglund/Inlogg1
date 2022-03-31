a4bfb1d22447c5c2e0262e08104a68aa20ae2b09
Cristian Berglund <Cristian.berglund@hotmail.se>
2022-03-31 18:47:56 +0200

    sdf

--- inlogg2/server/routes/auth.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { signup, login } = require("../controllers/authController");
-const router = express.Router();
-
-router.post("/signup", signup);
-router.post("/signin", login);
-
-module.exports = router;
